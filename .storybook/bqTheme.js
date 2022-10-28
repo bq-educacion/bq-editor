04148cda7d86f2e349a4870d115753805bd5223a
Carmen <carmen.dominguez@bq.com>
2022-10-28 11:54:47 +0200

    Add storbooky theme

---
+++ .storybook/bqTheme.js
@@ -0,0 +1,10 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "BQ Component Library",
+  brandUrl: "https://educacion.bq.com/",
+  brandImage:
+    "https://educacion.bq.com/wp-content/uploads/2020/11/BQ_Educacion_versiones-logo_rgb_fondo_colores_Mesa-de-trabajo-1-copia-10-3.png.webp",
+  brandTarget: "_self",
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import bqTheme from "./bqTheme";
+
+addons.setConfig({
+  theme: bqTheme,
+});
