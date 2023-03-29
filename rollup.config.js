af1f42f63cb91d8b4416b4e94837850afe07a261
Carmen <carmen.dominguez@bq.com>
2023-03-29 22:22:44 +0200

    Update rollup css plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
 import json from "@rollup/plugin-json";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import css from "rollup-plugin-import-css";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import styles from "rollup-plugin-styles";
 import tsConfig from "./tsconfig.json";
 import packageJson from "./package.json";
 
@@ -32,13 +32,24 @@ export default [
       typescript({
         sourceMap: tsConfig.compilerOptions.sourceMap,
       }),
-      css(),
       json(),
+      styles(),
+    ],
+    external: [
+      "refractor/lang/css.js",
+      "refractor/lang/javascript.js",
+      "refractor/lang/json.js",
+      "refractor/lang/markdown.js",
+      "refractor/lang/typescript.js",
     ],
   },
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: packageJson.types, format: "esm" }],
     plugins: [dts()],
+    external: [
+      "remirror/styles/extension-code-block.css",
+      "remirror/styles/extension-placeholder.css",
+    ],
   },
 ];
